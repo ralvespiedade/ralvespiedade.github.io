7eec5b555f6580400d76cda5d0e1dd6b957a1d0d
Ricardo A. P. Filho <60671960+ralvespiedade@users.noreply.github.com>
2023-04-12 17:09:58 -0300

    html js adicão de menu para cell

--- kr_pecas/script.js
+++ kr_pecas/script.js
@@ -25,6 +25,48 @@ for (i of opcoes_menu) {
 
 
 
+const Main = {
+    init: function () {
+        this.cacheSelector()
+        this.bindEvents()
+        
+    },
+    
+    cacheSelector: function () {
+        this.$button_ham = document.querySelector('.menu_hamburger')
+        this.$menu_cell = document.querySelector('.hamburger')
+        
+    },
+    
+    bindEvents: function () {
+        this.$button_ham.onclick = this.Events.open_menu_click.bind(this)
+        
+        
+    },
+
+    Events: {
+      open_menu_click: function(e) {
+        const menu_cell = this.$menu_cell
+
+        
+        if (!menu_cell.classList.contains('opened')) {
+            menu_cell.classList.add('opened')
+            return
+        }
+        menu_cell.classList.remove('opened')
+      }
+    }
+
+
+    
+}
+
+
+
+
+Main.init()
+
+
 
 
 
