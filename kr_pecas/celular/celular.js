c0a12cd11cfc8102e9e6726c35cd49f5b04d1a5e
Ricardo A. P. Filho <60671960+ralvespiedade@users.noreply.github.com>
2023-04-24 11:42:44 -0300

    ajuste no alinhamento das imagens do carrossel

--- kr_pecas/celular/celular.js
+++ kr_pecas/celular/celular.js
@@ -1,39 +1,61 @@
-//a idéia aqui é fazer a lógica do menu hamburger.
 
-//preciso fazer a criação dessa div que ficará posicionada fora da tela, quando o botão for clicado, essa div se moverá da direita pra esquerda surgindo com as opções do menu. 
-
-//tarefa 1: Criar div
 
 const Main = {
+
     init: function () {
         this.cacheSelector()
         this.bindEvents()
+        
         
     },
     
     cacheSelector: function () {
         this.$button_ham = document.querySelector('.menu_hamburger')
         this.$menu_cell = document.querySelector('.hamburger')
-        
+        this.$galeria = document.querySelector('.galeria')
+        this.$imgs = document.querySelectorAll('.galeria img')
     },
     
     bindEvents: function () {
         this.$button_ham.onclick = this.Events.open_menu_click.bind(this)
-        
-        
+        //this.$img.onclick = this.Events.rotation.bind(this)
+        this.Events.rotation(this.$imgs, this.$galeria)
     },
 
     Events: {
       open_menu_click: function(e) {
-        const menu_cell = this.$menu_cell
+            const menu_cell = this.$menu_cell
 
-        
-        if (!menu_cell.classList.contains('opened')) {
-            menu_cell.classList.add('opened')
-            return
+            if (!menu_cell.classList.contains('opened')) {
+                menu_cell.classList.add('opened')
+                return
+            }
+            menu_cell.classList.remove('opened')
+      },
+
+        rotation: function(imgs, galeria) {
+            let qtIMGs = 0
+
+            for (item of imgs) {
+                qtIMGs++
+            }
+
+            console.log(qtIMGs)
+
+            let cont = 0
+            
+            function carrossel () {
+                if (cont > qtIMGs - 1) {
+                    cont = 0
+                }
+                
+                galeria.style.transform = `translateX(${-cont * 355}px)`
+                
+                cont++
+            }    
+            
+            setInterval(carrossel, 3000)
         }
-        menu_cell.classList.remove('opened')
-      }
     }
 
 
--- kr_pecas/script.js
+++ kr_pecas/script.js
@@ -1,7 +1,6 @@
 lista_menu = document.querySelector('#lista_menu')
 
 
-
 opcoes_menu = [
     {nome:'NOSSA HISTORIA', id:'#about'},
     {nome:'PRODUTOS', id: '#products'},
@@ -10,12 +9,12 @@ opcoes_menu = [
 ]
 //só exibirá o menu quando a largura da tela for maior que 600px
 
-
 for (i of opcoes_menu) {
     
     
     lista_menu.innerHTML += `
         <li>
+            
             <a href=${i.id}>${i.nome}</a>
 
         </li>
@@ -35,12 +34,14 @@ const Main = {
     cacheSelector: function () {
         this.$button_ham = document.querySelector('.menu_hamburger')
         this.$menu_cell = document.querySelector('.hamburger')
-        
+        this.$galeria = document.querySelector('.galeria')
+        this.$imgs = document.querySelectorAll('.galeria img')
     },
+        
     
     bindEvents: function () {
         this.$button_ham.onclick = this.Events.open_menu_click.bind(this)
-        
+        this.Events.rotation(this.$imgs, this.$galeria)
         
     },
 
@@ -54,50 +55,35 @@ const Main = {
             return
         }
         menu_cell.classList.remove('opened')
-      }
+      }, 
+
+      rotation: function(imgs, galeria) {
+        var qtIMGs = 0
+        const screenWidth  = window.innerWidth
+
+        console.log(screenWidth)
+
+        for (item of imgs) {
+            qtIMGs++
+        }
+
+        let cont = 0
+        
+        function carrossel () {
+            if (cont > qtIMGs - 1) {
+                cont = 0
+            }
+            
+            galeria.style.transform = `translateX(${-cont * screenWidth}px)`
+            
+            cont++
+        }    
+        
+        setInterval(carrossel, 3000)
     }
 
-
+    }
     
 }
-
-
-
 
 Main.init()
-
-
-
-
-
-/* 
-let diaSemana = new Date().getDay()
-switch(diaSemana) {
-    case 0:
-        console.log("Hoje é domingo")
-        break;
-    case 1:
-        console.log("Hoje é segunda")
-        break;
-    case 2:
-        console.log("Hoje é terça")
-        break;
-    case 3:
-        console.log("Hoje é quarta")
-        break;
-    case 4:
-        console.log("Hoje é quinta")
-}
-
-
-
-if (media >= 7) {
-    
-    console.log(`Sua média é de ${media}, você foi aprovado`)
-
-} else if (media >= 5){
-    console.log(`Sua média é de ${media}, você está de recuperação`)
-} else {
-    console.log(`Sua média é de ${media}, você foi reprovado`)
-}
-*/
