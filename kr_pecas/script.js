45143d06eec6803c60313b495dce4e6a77091276
Ricardo A. P. Filho <60671960+ralvespiedade@users.noreply.github.com>
2023-04-25 14:58:55 -0300

    links do menu hamburger

--- kr_pecas/script.js
+++ kr_pecas/script.js
@@ -36,54 +36,65 @@ const Main = {
         this.$menu_cell = document.querySelector('.hamburger')
         this.$galeria = document.querySelector('.galeria')
         this.$imgs = document.querySelectorAll('.galeria img')
+        this.$listaLiMenuHamburger = document.querySelectorAll('.hamburger ul li')
+        this.Events.rotation(this.$imgs, this.$galeria)
+        //this.$btQuemSomos = document.querySelector('#quemsomos')
+        //this.$btContatos = document.querySelector('#contatos')
+        //this.$localizacao = document.querySelector('#localizacao')
     },
         
     
     bindEvents: function () {
         this.$button_ham.onclick = this.Events.open_menu_click.bind(this)
-        this.Events.rotation(this.$imgs, this.$galeria)
+        this.$listaLiMenuHamburger.forEach(element => { 
+            element.onclick = this.Events.open_menu_click.bind(this)
+            console.log(element)
+        })
+        //this.$btQuemSomos.onclick = this.Events.open_menu_click.bind(this)
         
     },
 
     Events: {
-      open_menu_click: function(e) {
-        const menu_cell = this.$menu_cell
-
-        
-        if (!menu_cell.classList.contains('opened')) {
-            menu_cell.classList.add('opened')
-            return
-        }
-        menu_cell.classList.remove('opened')
-      }, 
-
-      rotation: function(imgs, galeria) {
-        var qtIMGs = 0
-        const screenWidth  = window.innerWidth
-
-        console.log(screenWidth)
-
-        for (item of imgs) {
-            qtIMGs++
-        }
-
-        let cont = 0
-        
-        function carrossel () {
-            if (cont > qtIMGs - 1) {
-                cont = 0
+        open_menu_click: function() {
+            const menu_cell = this.$menu_cell
+            
+            if (!menu_cell.classList.contains('opened')) {
+                menu_cell.classList.add('opened')
+                return
             }
+
+            menu_cell.classList.remove('opened')
+        }, 
+
+        rotation: function(imgs, galeria) {
+            var qtIMGs = 0
+            const screenWidth  = window.innerWidth
+
+            console.log(screenWidth)
+
+            for (item of imgs) {
+                qtIMGs++
+            }
+
+            let cont = 0
             
-            galeria.style.transform = `translateX(${-cont * screenWidth}px)`
+            function carrossel () {
+                if (cont > qtIMGs - 1) {
+                    cont = 0
+                }
+                
+                galeria.style.transform = `translateX(${-cont * screenWidth}px)`
+                
+                cont++
+            }    
             
-            cont++
-        }    
-        
-        setInterval(carrossel, 3000)
-    }
+            setInterval(carrossel, 3000)
+        },
 
     }
-    
+        
 }
+    
+    
 
 Main.init()
